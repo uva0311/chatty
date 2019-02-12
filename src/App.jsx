d965906c6772c9b938b5f9700daaf44cba29bbd2
Arthur Lam <lam.arthur0311@gmail.com>
2019-02-12 16:19:00 +0000

    modified to include ChatBar, Message components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Message from './Message.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <body>
+        <ChatBar />
+        <Message />
+      </body>
     );
   }
 }
