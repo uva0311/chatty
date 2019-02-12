800a19b016174af03116c3d97ed481503abd9f29
Arthur Lam <lam.arthur0311@gmail.com>
2019-02-12 16:20:44 +0000

    components created

---
+++ src/MessageList.jsx
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  constructor() {
+    super();
+    this.state.messages = [
+        {
+          type: "incomingMessage",
+          content: "I won't be impressed with technology until I can download food.",
+          username: "Anonymous1"
+        },
+        {
+          type: "incomingNotification",
+          content: "Anonymous1 changed their name to nomnom",
+        },
+        {
+          type: "incomingMessage",
+          content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+          username: "Anonymous2"
+        },
+        {
+          type: "incomingMessage",
+          content: "...",
+          username: "nomnom"
+        },
+        {
+          type: "incomingMessage",
+          content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
+          username: "Anonymous2"
+        },
+        {
+          type: "incomingMessage",
+          content: "This isn't funny. You're not funny",
+          username: "nomnom"
+        },
+        {
+          type: "incomingNotification",
+          content: "Anonymous2 changed their name to NotFunny",
+        },
+    ];
+  }
+
+  const notice = this.state.messages.filter((message) =>
+    return message.type === 'incomingNotification');
+
+  const message = this.state.messages.filter((message) =>
+    return message.type === 'incomingMessage');
+
+  render() {
+    return(
+
+    );
+  }
+}
+
+export default MessageList;
